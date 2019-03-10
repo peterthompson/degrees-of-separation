886767dde22773620bdeb3437473f5315f428f4e
Pete Thompson <me@petethompson.com>
2019-03-10 09:39:20 +0000

    Add implementation for redux functions

--- src/redux.js
+++ src/redux.js
@@ -10,8 +10,40 @@ export const initialState = {
   movies: []
 };
 
-export const moviesReducer = (state = initialState, action = {}) => {};
+export const moviesReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case GET_MOVIES_REQUEST:
+      return {
+        isFetching: true,
+        movies: []
+      };
+    case GET_MOVIES_SUCCESS:
+      return {
+        isFetching: false,
+        movies: action.payload
+      };
+    case GET_MOVIES_FAILURE:
+      return {
+        isFetching: false,
+        movies: []
+      };
+    default:
+      return state;
+  }
+};
 
-export function getMovies() {}
+export function getMovies() {
+  return async dispatch => {
+    try {
+      dispatch({ type: GET_MOVIES_REQUEST });
+      const response = await fetch(config.moviesUrl);
+      if (!response.ok) throw new Error("Invalid Response");
+      const json = await response.json();
+      dispatch({ type: GET_MOVIES_SUCCESS, payload: json });
+    } catch (error) {
+      dispatch({ type: GET_MOVIES_FAILURE, error });
+    }
+  };
+}
 
 export default moviesReducer;
